abe8f7c0df6aab3c8192c9ab68cb6c5d119de98e
Lucas Rodriguesm <lalves547@gmail.com>
2024-11-09 21:52:08 -0300

    init db connection

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const express = require('express');
+import { Sequelize, DataTypes } from 'sequelize';
+import express from 'express';
 
 const app = express();
 const port = 8000;
+const address = 'http://localhost:8000';
 
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: ':memory:'
+});
+const URLS = sequelize.define('URLS', {
+  url: DataTypes.STRING,
+  shorted: DataTypes.STRING,
+  expiresAt: DataTypes.DATE
+});
+
+sequelize.sync();
+
+app.use(express.json());
+
+app.post('/shorten-url', async (req, res) => {
+    const { url } = req.body;
+    const generateShortCode = (length) => {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+    };
+
+    const shorted = generateShortCode(Math.floor(Math.random() * 6) + 5);
+    const result = await URLS.create({ url, shorted, expiresAt: new Date(Date.now() + 60 * 60 * 1000) });
+    res.json({url: `${ address }/${ result.shorted }`});
+});
 app.post('/shorten-url', (req, res) => {
     url = req.body.url;
     res.send('Shortened URL: http://localhost:8000/abc123');
